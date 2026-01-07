42ab37a0f0dc54a4d1185c04ea2d36ebec8e3cde
Vansh2693 <vanshraj2693@gmail.com>
2026-01-08 02:05:25 +0530

    Added Frontend for the project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#a855f7",   
+        secondary: "#f5f3ff",
+      },
+    },
+  },
+  plugins: [],
+};
